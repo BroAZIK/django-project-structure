4ae670e6cda3f603ef9ec9019712ebf39e3d5d2b
djumanov <diyorbekjumanov1120@gmail.com>
2023-10-12 10:46:07 +0500

    request.body ;

--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpRequest, HttpResponse
+import json
 
 
 def test_view(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":
+        pass
+    
+    if request.method == 'POST':
+
+        body = request.body.decode()
+        data = json.loads(body)
+        print(data)
+        print(type(data))
 
     return HttpResponse("ok")
-    
